12c7cf9a79c435b2f9d4f810f5c56fa8af99c31a
KopylashTetiana <kopylashtetiana@gmail.com>
2023-02-01 14:50:55 +0200

    BufferedWriter added; writer, socket and server closed

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,12 +1,26 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) throws IOException {
 
         ServerSocket server = new ServerSocket(8000);
         System.out.println("Server started!");
-        server.accept();
+
+        Socket socket = server.accept();
         System.out.println("Client connected");
+
+        BufferedWriter writer =
+                new BufferedWriter(
+                        new OutputStreamWriter(
+                                socket.getOutputStream()));
+        writer.write("Hello from server");
+        writer.newLine();
+        writer.flush();
+
+        writer.close();
+        socket.close();
+        server.close();
     }
 }
