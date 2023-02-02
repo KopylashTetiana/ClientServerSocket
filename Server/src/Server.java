e7ef83a15507b829bcf0b817bbe4237d08857505
KopylashTetiana <kopylashtetiana@gmail.com>
2023-02-02 12:58:32 +0200

    try-with-resources, BufferedReader and BufferedWriter, request and response added

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -5,22 +5,29 @@ import java.net.Socket;
 public class Server {
     public static void main(String[] args) throws IOException {
 
-        ServerSocket server = new ServerSocket(8000);
-        System.out.println("Server started!");
-
-        Socket socket = server.accept();
-        System.out.println("Client connected");
-
-        BufferedWriter writer =
-                new BufferedWriter(
-                        new OutputStreamWriter(
-                                socket.getOutputStream()));
-        writer.write("Hello from server");
-        writer.newLine();
-        writer.flush();
-
-        writer.close();
-        socket.close();
-        server.close();
+        try (ServerSocket server = new ServerSocket(8000);) {
+            System.out.println("Server started!");
+            try (
+                Socket socket = server.accept();
+                BufferedWriter writer =
+                    new BufferedWriter(
+                            new OutputStreamWriter(
+                                    socket.getOutputStream()));
+                BufferedReader reader =
+                    new BufferedReader(
+                            new InputStreamReader(
+                                    socket.getInputStream()));)
+            {
+                String request = reader.readLine();
+                System.out.println("Request: " + request);
+                String response = "Hello from server: " + request.length();
+                System.out.println("Response: " + response);
+                writer.write(response);
+                writer.newLine();
+                writer.flush();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
